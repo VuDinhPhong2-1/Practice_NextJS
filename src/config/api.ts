528d1a889232c9d47df436dd5d6028bc62914139
BrianVu <d.bed12@gptgdt.net>
2023-11-02 12:29:37 +0700

    Initial commit

---
+++ src/config/api.ts
@@ -0,0 +1,111 @@
+import { IBackendRes, ICompany, IAccount, IUser, IModelPaginate, IGetAccount, IJob } from '@/types/backend';
+import axios from 'config/axios-customize';
+
+/**
+ * 
+Module Auth
+ */
+export const callRegister = (name: string, email: string, password: string, age: number, gender: string, address: string) => {
+    return axios.post<IBackendRes<IUser>>('/api/v1/auth/register', { name, email, password, age, gender, address })
+}
+
+export const callLogin = (username: string, password: string) => {
+    return axios.post<IBackendRes<IAccount>>('/api/v1/auth/login', { username, password })
+}
+
+export const callFetchAccount = () => {
+    return axios.get<IBackendRes<IGetAccount>>('/api/v1/auth/account')
+}
+
+export const callRefreshToken = () => {
+    return axios.get<IBackendRes<IAccount>>('/api/v1/auth/refresh')
+}
+
+export const callLogout = () => {
+    return axios.post<IBackendRes<string>>('/api/v1/auth/logout')
+}
+
+/**
+ * Upload single file
+ */
+export const callUploadSingleFile = (file: any, folderType: string) => {
+    const bodyFormData = new FormData();
+    bodyFormData.append('fileUpload', file);
+    return axios<IBackendRes<{ fileName: string }>>({
+        method: 'post',
+        url: '/api/v1/files/upload',
+        data: bodyFormData,
+        headers: {
+            "Content-Type": "multipart/form-data",
+            "folder_type": folderType
+        },
+    });
+}
+
+
+
+
+/**
+ * 
+Module Company
+ */
+export const callCreateCompany = (name: string, address: string, description: string, logo: string) => {
+    return axios.post<IBackendRes<ICompany>>('/api/v1/companies', { name, address, description, logo })
+}
+
+export const callUpdateCompany = (id: string, name: string, address: string, description: string, logo: string) => {
+    return axios.patch<IBackendRes<ICompany>>(`/api/v1/companies/${id}`, { name, address, description, logo })
+}
+
+export const callDeleteCompany = (id: string) => {
+    return axios.delete<IBackendRes<ICompany>>(`/api/v1/companies/${id}`);
+}
+
+export const callFetchCompany = (query: string) => {
+    return axios.get<IBackendRes<IModelPaginate<ICompany>>>(`/api/v1/companies?${query}`);
+}
+
+
+/**
+ * 
+Module User
+ */
+export const callCreateUser = (user: IUser) => {
+    return axios.post<IBackendRes<IUser>>('/api/v1/users', { ...user })
+}
+
+export const callUpdateUser = (user: IUser) => {
+    return axios.patch<IBackendRes<IUser>>(`/api/v1/users`, { ...user })
+}
+
+export const callDeleteUser = (id: string) => {
+    return axios.delete<IBackendRes<IUser>>(`/api/v1/users/${id}`);
+}
+
+export const callFetchUser = (query: string) => {
+    return axios.get<IBackendRes<IModelPaginate<IUser>>>(`/api/v1/users?${query}`);
+}
+
+/**
+ * 
+Module Job
+ */
+export const callCreateJob = (job: IJob) => {
+    return axios.post<IBackendRes<IJob>>('/api/v1/jobs', { ...job })
+}
+
+export const callUpdateJob = (job: IJob, id: string) => {
+    return axios.patch<IBackendRes<IJob>>(`/api/v1/jobs/${id}`, { ...job })
+}
+
+export const callDeleteJob = (id: string) => {
+    return axios.delete<IBackendRes<IJob>>(`/api/v1/jobs/${id}`);
+}
+
+export const callFetchJob = (query: string) => {
+    return axios.get<IBackendRes<IModelPaginate<IJob>>>(`/api/v1/jobs?${query}`);
+}
+
+export const callFetchJobById = (id: string) => {
+    return axios.get<IBackendRes<IJob>>(`/api/v1/jobs/${id}`);
+}
---
+++ src/redux/store.ts
@@ -0,0 +1,28 @@
+import {
+  Action,
+  configureStore,
+  ThunkAction,
+} from '@reduxjs/toolkit';
+import accountReducer from './slice/accountSlide';
+import companyReducer from './slice/companySlide';
+import userReducer from './slice/userSlide';
+import jobReducer from './slice/jobSlide';
+
+export const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    company: companyReducer,
+    user: userReducer,
+    job: jobReducer,
+  },
+});
+
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
